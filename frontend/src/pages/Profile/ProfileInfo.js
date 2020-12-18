800d0b3b82fe4112a867c54f192735f8f15bf9c8
Josh Kaemmerer <josh.kaemmerer@basisesports.com>
2020-12-17 18:15:58 -0600

    Update ProfileInfo.js
    Fixed activeclassname that was throwing an error

--- frontend/src/pages/Profile/ProfileInfo.js
+++ frontend/src/pages/Profile/ProfileInfo.js
@@ -133,7 +133,7 @@ const ProfileInfo = ({ user }) => {
           )}
         </FullName>
         <Buttons>
-          <Link exact activeClassName="selected" to={generatePath(Routes.EDIT)}>
+          <Link exact activeclassname="selected" to={generatePath(Routes.EDIT)}>
             <Button text type="button">
               Edit Profile
             </Button>
